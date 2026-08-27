9960274542efa89442b0f609d2046f57d5c7d864
dennismogaka76 <dennismogaka76@gmail.com>
2026-08-27 10:51:53 +0300

    updated database settings in settings.py

--- config/settings.py
+++ config/settings.py
@@ -87,11 +87,7 @@ DATABASES = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DATABASE_NAME'),
-        'USER': config('DATABASE_USER'),
-        'PASSWORD': config('DATABASE_PASSWORD'),
-        'HOST': config('DATABASE_HOST'),
-        'PORT': config('DATABASE_PORT'),
+
     }
 }
 STATIC_URL = "/static/"
