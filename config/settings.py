1a27a19f3ebdef5a0a52ce6275fef40576311916
dennismogaka76 <dennismogaka76@gmail.com>
2026-08-27 10:49:49 +0300

    added psycopg and updated database settings in settings.py

--- config/settings.py
+++ config/settings.py
@@ -81,8 +81,18 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=config('DATABASE_URL')
+        
     )
+}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST'),
+        'PORT': config('DATABASE_PORT'),
+    }
 }
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
